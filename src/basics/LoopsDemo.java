2bb9c3a1af1f30674806cd01e1abe72d823a221b
automationtrainingcenter <atcsurya@gmail.com>
2020-04-02 08:16:14 +0530

    break and continue with inner loops

--- src/basics/LoopsDemo.java
+++ src/basics/LoopsDemo.java
@@ -46,6 +46,15 @@ for(array_data_type var_name : array_var_name){
 }
 
 
+break: is used to terminate the loop based on some condition. Once break statement
+is executed, then the other statements after the break statement within the loop
+will not execute and also it won't repeat next iterations.
+
+continue: is used to skip the current iteration. Once continue statement is executed,
+then the other statements after the continue statement within the loop will not execute
+but it will execute next iterations.
+
+
  */
 
 import java.util.Scanner;
@@ -58,7 +67,7 @@ public class LoopsDemo {
         // initialization
         int i = 1;
         // condition or expression
-        while(i < 11){
+        while (i < 11) {
             // statements to repeat
             System.out.println(i);
             // increment
@@ -72,18 +81,18 @@ public class LoopsDemo {
         System.out.println("****Do While loop****");
 
         int j = 20;
-        do{
-            if(j % 2 == 0){
+        do {
+            if (j % 2 == 0) {
                 System.out.println(j);
             }
             j--; // decrement
-        }while(j > 10); // condition
+        } while (j > 10); // condition
         // Note: The minimum number of iterations for do while loop is 1
 
 
         // for loop: print 3 multiples from 1 to 10
         System.out.println("****For loop****");
-        for(int k = 1; k < 11; k++){
+        for (int k = 1; k < 11; k++) {
             System.out.println(k * 3);
         }
         // Note: The minimum number of iterations for the for loop is 0
@@ -92,7 +101,7 @@ public class LoopsDemo {
         // for each loop: to retrieve values from an array
         System.out.println("****For each loop****");
         int[] numbers = {2, 3, 5, 7, 11, 13, 17, 19};
-        for(int number : numbers){
+        for (int number : numbers) {
             System.out.println(number);
         }
 
@@ -110,29 +119,89 @@ public class LoopsDemo {
 //        }
 
         String input = "";
-        do{
-            System.out.println("input string is :: "+input);
+        do {
+            System.out.println("input string is :: " + input);
             System.out.println("enter some string");
             input = sc.next();
-        } while(!input.equalsIgnoreCase("exit"));
+        } while (!input.equalsIgnoreCase("exit"));
 
         // reverse the given string
         // verify a string is palindrome or not
         String data = "madam";
         String revData = "";
-        for(int s = data.length() -1; s >= 0 ; s--){
+        for (int s = data.length() - 1; s >= 0; s--) {
             revData = revData + data.charAt(s);
         }
         System.out.println(revData);
 
         // check palindrome
-        if(data.equalsIgnoreCase(revData)){
+        if (data.equalsIgnoreCase(revData)) {
             System.out.println("given string is palindrome");
-        }else{
+        } else {
             System.out.println("given string is not a palindrome");
         }
 
+        System.out.println("****************** Break **********************");
+        // break statement
+        for (int l = 0; l < 10; l++) {
+            if (l == 5) {
+                break;
+            }
+            System.out.print(l + "\t");
+        }
 
+
+        System.out.println("\n****************** Continue **********************");
+        // Continue statement
+        for (int l = 0; l < 10; l++) {
+            if (l == 5) {
+                continue;
+            }
+            System.out.print(l + "\t");
+        }
+
+        System.out.println("\n**************** Continue 2 ***********************");
+        // print even number from 1 to 11 using continue keyword
+        for (int l = 1; l < 11; l++) {
+            if (l % 2 == 1) {
+                continue;
+            }
+            System.out.println(l);
+        }
+
+        // verify a given number is prime or not
+        // A prime number is a whole number greater than 1 whose only factors are 1 and itself
+        int number = 11;
+        boolean flag = true;
+        for (int l = 2; l <= number / 2; l++) {
+            if (number % l == 0) {
+                flag = false;
+                break; // terminate for loop
+            }
+        }
+        if (flag) {
+            System.out.println(number + " is prime");
+        } else {
+            System.out.println(number + " is not prime");
+        }
+
+        // read the numbers from the console until we enter a prime number
+        int primeOrNot = 0;
+        boolean status;
+        do {
+            System.out.println("enter some number");
+            primeOrNot = sc.nextInt();
+            // verify given number is prime number or not
+            status = true;
+            for (int l = 2; l <= primeOrNot / 2; l++) {
+                if (primeOrNot % l == 0) {
+                    System.out.println(primeOrNot);
+                    status = false;
+                    break; // terminates the for loop
+                }
+            }
+
+        } while (!status);
 
 
     }
