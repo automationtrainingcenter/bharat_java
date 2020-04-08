8e0545af1b82485e79b7181dbf191b8d5e620a58
automationtrainingcenter <atcsurya@gmail.com>
2020-04-08 08:13:33 +0530

    encapsulation and inheritance intro

--- src/encapsulation/EncapsulationDemo.java
+++ src/encapsulation/EncapsulationDemo.java
@@ -2,15 +2,26 @@ package encapsulation;
 /*
     Encapsulation means hiding the properties of a class outside of the class or
     Encapsulation means binding data and the operations on the data together.
+
+    to hide the properties outside the class we have to declare those
+    properties using private access modifier.
+
+    to perform read and write operations on the properties we have to create
+    getter and setter methods using public access modifier.
+
+    read --- getter
+    write --- setter
  */
+
+import java.util.Scanner;
 
 public class EncapsulationDemo {
 
-    int accNum;
-    String name;
-    String address;
-    long phoneNum;
-    double balance;
+    private int accNum;
+    private String name;
+    private String address;
+    private long phoneNum;
+    private double balance;
 
     // parameterized constructor
     public EncapsulationDemo(int accNum, String name, String address, long phoneNum, double balance) {
@@ -25,4 +36,39 @@ public class EncapsulationDemo {
         System.out.println("account number = " + accNum + "\tname = " + name + "\taddress = " + address + "\tphone number = " + phoneNum + "\tbalance = " + balance);
     }
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your phone number");
+        long pn = sc.nextLong();
+        if(this.phoneNum == pn) {
+            this.address = address;
+            System.out.println("address updated successfully");
+        }else{
+            System.out.println("Unauthorized access. Invalid phone number");
+        }
+    }
+
+    public long getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(long phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public int getAccNum() {
+        return accNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
--- src/encapsulation/TestEncapuslationDemo.java
+++ src/encapsulation/TestEncapuslationDemo.java
@@ -4,9 +4,17 @@ public class TestEncapuslationDemo {
 
     public static void main(String[] args) {
         EncapsulationDemo obj = new EncapsulationDemo(1010, "surya", "lingampalli", 9876543210l, 10000);
-        obj.name = "susnhine";
-        obj.balance = 90000;
-
+//        obj.name = "susnhine";
+//        obj.balance = 90000;
+        System.out.println("Before Update :: ");
+        System.out.println(obj.getAddress());
+        obj.setAddress("miyapur");
+        System.out.println(obj.getPhoneNum());
+        obj.setPhoneNum(7896612345l);
+        System.out.println(obj.getName());
+        System.out.println(obj.getAccNum());
+        System.out.println(obj.getBalance());
+        System.out.println("After Update ::");
         obj.display();
     }
 }
